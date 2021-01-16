1ec5ca475cced4d774e9bfdbf6c831210be7537c
Peter Ardiarta <50816765+prooholic@users.noreply.github.com>
2021-01-16 17:30:39 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export KERNELNAME="Prooholic-Kranul"
+
+export LOCALVERSION=v2
+
+export KBUILD_BUILD_USER=Peter
+
+export KBUILD_BUILD_HOST=DroneCI
+
+export TOOLCHAIN=clang
+
+export DEVICES=whyred
+
+source helper
+
+gen_toolchain
+
+send_msg "Start building ${KERNELNAME} ${LOCALVERSION} for ${DEVICES}..."
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do
+	build ${i} -oldcam
+
+	build ${i} -newcam
+done
+
+send_msg "Building OC version..."
+
+git apply oc.patch
+
+for i in ${DEVICES//,/ }
+do
+	if [ $i == "whyred" ] || [ $i == "tulip" ]
+	then
+		build ${i} -oldcam -overclock
+
+		build ${i} -newcam -overclock
+	fi
+done
+
+END=$(date +"%s")
+
+DIFF=$(( END - START ))
+
+send_msg "Build succesfully in $((DIFF / 60))m $((DIFF % 60))s"
